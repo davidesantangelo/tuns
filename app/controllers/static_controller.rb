1c3c782167da231dcfbcbfc75c027c244e0a6a6e
Davide Santangelo <davide.santangelo@gmail.com>
2015-06-01 17:20:48 +0200

    init stats privacy for cookie law

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,13 @@
 class StaticController < ApplicationController
   def index
   end
+
+  def privacy
+    cookies[:acceptterms] = true
+  end
+
+  def acceptterms
+  	cookies[:acceptterms] = true
+  	redirect_to :back
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,16 @@ Rails.application.routes.draw do
   get '/about' => 'static#about', :as => :about
   get '/thanks' => 'static#thanks', :as => :thanks
   get '/feedbacks' => 'static#feedbacks', :as => :feedbacks
+  get '/privacy' => 'static#privacy', :as => :privacy
+  post '/acceptterms' => 'static#acceptterms', :as => :acceptterms
 
   resources :users, :path => "/" do
     collection do 
       match ':id/complete' => 'users#complete', via: [:get, :patch], :as => :complete
-      match 'loadmore' => 'users#loadmore', via: [:get]
-      match ':id/donate' => 'users#donate', via: [:get], :as => :donate
-      match 'stats' => 'users#stats', via: [:get], :as => :stats
+      get 'loadmore' => 'users#loadmore', via: [:get]
+      get ':id/donate' => 'users#donate', :as => :donate
+      get 'stats' => 'users#stats', :as => :stats
+      get 'loadstats' => 'users#loadstats'
     end
   end
 end
