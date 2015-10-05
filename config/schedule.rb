7622160a416752d0e1c0e9dae4594e5b3fddf8cd
Davide Santangelo <davide.santangelo@gmail.com>
2015-10-05 10:35:02 +0200

    fix cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,3 @@
-job_type :rake_with_lock, "cd :path && export :environment_variable=:environment && /usr/bin/flock -n /tmp/:task.lock bundle exec rake :task --silent :output"
-
-env :MAILTO, 'davide.santangelo@gmail.com'
-env :PATH, ENV['PATH']
-
 every 8.hours do
-  rake_with_lock "users:unfollowers"
+  rake "users:unfollowers"
 end
