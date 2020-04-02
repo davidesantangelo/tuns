f7d7cef1cc5a286441b02229e18a59da90a140a1
Davide Santangelo <davide.santangelo@gmail.com>
2020-04-02 12:37:08 +0200

    fix domain

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ActionMailer::Base
   def unfollower(unfollower)
     @unfollower = unfollower
     @user = unfollower.user
-    mail(from: "\"#{@unfollower.name} (via TUNS)\" <notify@tuns.it>",
+    mail(from: "\"#{@unfollower.name} (via TUNS)\" <notify@tunsapp.com>",
          to: "#{@user.name} <#{@user.email}>",
          subject: "#{@unfollower.name} (@#{@unfollower.username}) has just unfollowed you on Twitter!",
          layout: 'mail')
@@ -15,7 +15,7 @@ class UserMailer < ActionMailer::Base
   def follower(follower)
     @follower = follower
     @user = follower.user
-    mail(from: "\"#{@follower.name} (via TUNS)\" <notify@tuns.it>",
+    mail(from: "\"#{@follower.name} (via TUNS)\" <notify@tunsapp.com>",
          to: "#{@user.name} <#{@user.email}>",
          subject: "#{@follower.name} (@#{@follower.username}) has just returned to follow you on Twitter!",
          layout: 'mail')
